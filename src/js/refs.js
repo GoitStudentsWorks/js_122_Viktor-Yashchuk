fef418128d12fcfa64ef2d85086a5589cea9aa32
Uffaser <gerasimenkom405@gmail.com>
2025-12-17 23:52:18 +0200

    Add success section with styling, functionality, and SVG icons

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+    successList: document.querySelector('.success-list'),
+    successBtnForward: document.querySelector('.success-button-forward'),
+    successBtnBack: document.querySelector('.success-button-back'),
+    successAnimation: document.querySelector('.ajax-loader'),
+};
